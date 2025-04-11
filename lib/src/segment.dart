e7f5d8b5d5f59baa77798533a574ccb7a98c9445
Mahdi Pishguy <pishguy@gmail.com>
2025-04-11 14:36:49 +0330

    Update segment.dart

--- lib/src/segment.dart
+++ lib/src/segment.dart
@@ -7,17 +7,6 @@ part 'story_segment_wrapper.dart';
 /// A widget that displays a circular segmented border around an image,
 /// commonly used for story indicators.
 class StorySegmentView extends StatelessWidget {
-  /// Creates a [StorySegmentView] widget.
-  ///
-  /// [imageUrl] is the URL of the image to be displayed at the center.
-  /// [segments] defines the number of segments in the circular border.
-  /// [radius] sets the size of the outer circle.
-  /// [strokeWidth] determines the thickness of the segment lines.
-  /// [color] specifies the color of the segments.
-  /// Optional parameters:
-  /// - [segmentGap] controls the gap between segments (default: 2.0).
-  /// - [placeholder] is a widget displayed while the image loads.
-  /// - [errorWidget] is a widget displayed if the image fails to load.
   const StorySegmentView({
     required this.imageUrl,
     required this.segments,
@@ -28,31 +17,22 @@ class StorySegmentView extends StatelessWidget {
     this.segmentGap = 2.0,
     this.errorWidget,
     this.placeholder,
+    this.isAnimated = true,
+    this.rotationDuration = const Duration(seconds: 5),
+    this.padding = 4.0, // آرگومان جدید برای فاصله از مرکز
   });
 
-  /// The URL of the avatar image.
   final String imageUrl;
-
-  /// The number of segments in the circular indicator.
   final int segments;
-
-  /// The radius of the outer segment circle.
   final double radius;
-
-  /// The thickness of each segment line.
   final double strokeWidth;
-
-  /// The gap between each segment.
   final double segmentGap;
-
-  /// The color of the segment lines.
   final Color color;
-
-  /// Widget displayed while the image is loading.
   final Widget Function(BuildContext, String)? placeholder;
-
-  /// Widget displayed if the image fails to load.
   final Widget Function(BuildContext, String, Object)? errorWidget;
+  final bool isAnimated;
+  final Duration rotationDuration;
+  final double padding; // فاصله سگمنت‌ها از دایره مرکزی
 
   @override
   Widget build(BuildContext context) {
@@ -65,11 +45,14 @@ class StorySegmentView extends StatelessWidget {
           radius: radius,
           strokeWidth: strokeWidth,
           gap: segmentGap,
+          isAnimated: isAnimated,
+          rotationDuration: rotationDuration,
+          padding: padding,
         ),
         Container(
           width: (radius * 2) - 8,
           height: (radius * 2) - 8,
-          decoration: BoxDecoration(
+          decoration: const BoxDecoration(
             shape: BoxShape.circle,
           ),
           child: ClipRRect(
@@ -85,4 +68,5 @@ class StorySegmentView extends StatelessWidget {
       ],
     );
   }
+}
 }
