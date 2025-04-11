3825bff031a5659fb33768f26d26f48b8fbaf7c2
Mahdi Pishguy <pishguy@gmail.com>
2025-04-11 14:37:13 +0330

    Update story_segment_wrapper.dart

--- lib/src/story_segment_wrapper.dart
+++ lib/src/story_segment_wrapper.dart
@@ -1,12 +1,15 @@
 part of 'segment.dart';
 
-class _StorySegmentWrapper extends StatelessWidget {
+class _StorySegmentWrapper extends StatefulWidget {
   const _StorySegmentWrapper({
     required this.segments,
     required this.radius,
     required this.strokeWidth,
     required this.color,
     this.gap = 2.0,
+    required this.isAnimated,
+    required this.rotationDuration,
+    required this.padding,
   });
 
   final int segments;
@@ -14,18 +17,68 @@ class _StorySegmentWrapper extends StatelessWidget {
   final double strokeWidth;
   final double gap;
   final Color color;
+  final bool isAnimated;
+  final Duration rotationDuration;
+  final double padding;
+
+  @override
+  State<_StorySegmentWrapper> createState() => _StorySegmentWrapperState();
+}
+
+class _StorySegmentWrapperState extends State<_StorySegmentWrapper>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: widget.rotationDuration,
+    );
+    if (widget.isAnimated) {
+      _controller.repeat();
+    }
+  }
+
+  @override
+  void didUpdateWidget(_StorySegmentWrapper oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.isAnimated != oldWidget.isAnimated) {
+      if (widget.isAnimated) {
+        _controller.repeat();
+      } else {
+        _controller.stop();
+        _controller.value = 0;
+      }
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return CustomPaint(
-      size: Size(radius * 2, radius * 2),
-      painter: _PartialCirclePainter(
-        color: color,
-        segments: segments,
-        radius: radius,
-        strokeWidth: strokeWidth,
-        gap: gap,
-      ),
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (context, child) {
+        return CustomPaint(
+          size: Size(widget.radius * 2, widget.radius * 2),
+          painter: _PartialCirclePainter(
+            color: widget.color,
+            segments: widget.segments,
+            radius: widget.radius,
+            strokeWidth: widget.strokeWidth,
+            gap: widget.gap,
+            rotationAngle:
+            widget.isAnimated ? _controller.value * 2 * 3.14159265359 : 0.0,
+            padding: widget.padding,
+          ),
+        );
+      },
     );
   }
 }
